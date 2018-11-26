3bfb0dc746d25b0b7a95c4f3bbdb535c9324f67f
PostolatiyRose <rozalya16@gmail.com>
2018-11-26 14:08:08 +0200

    Initial commit

---
+++ src/test/java/onfreetube/FirstAction.java
@@ -0,0 +1,9 @@
+package onfreetube;
+
+public class FirstAction {
+    public static void Main(String[] args ) {
+        System.out.println("Hello world, iI`m Rose");
+    }
+   // private static WebDriver driver;
+   // System.setproperty("webdriver.chrome.driver", "chromedriver.exe");
+}
---
+++ src/test/java/onfreetube/test2.java
@@ -0,0 +1,78 @@
+package onfreetube;
+
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test2 {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new ChromeDriver();
+        baseUrl = "https://www.katalon.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        driver.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0");
+        driver.findElement(By.id("searchInput")).click();
+        driver.findElement(By.id("searchInput")).clear();
+        driver.findElement(By.id("searchInput")).sendKeys("crea");
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Crea'])[2]/following::div[1]")).click();
+        driver.findElement(By.linkText("Apple II")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Перейти к поиску'])[1]/following::img[1]")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
